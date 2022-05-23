fe206c3b237dfae2fff22499bcb4e38272b1aa78
Hendrik Cech <hendrik.cech@gmail.com>
2022-05-23 16:43:59 +0200

    Add changes

--- rfc8888.lua
+++ rfc8888.lua
@@ -18,13 +18,14 @@ do
     rfc_pkts_block_f = ProtoField.none("rfc8888.pkts_block", "Packet Feedback")
     rfc_pkt_block_f = ProtoField.string("rfc8888.pkt_block", "Packet")
     rfc_pkt_rcvd_f = ProtoField.bool("rfc8888.pkt_rcvd", "Packet received")
+    rfc_pkt_ecn_f = ProtoField.uint8("rfc8888.pkt_ecn", "Packet ECN flag")
     rfc_pkt_arrival_offset_f = ProtoField.float("rfc8888.pkt_arrival_offset", "Packet arrival offset (ms)")
 
     rfc_report_ts_f = ProtoField.float("rfc8888.report_ts", "Report timestamp")
 
     rfc_proto.fields = { rfc_version_f, rfc_padding_f, rfc_fmt_f, rfc_pt_f, rfc_length_f, rfc_ssrc_rtcp_f,
                          rfc_ssrcs_block_rtp_f, rfc_ssrc_block_rtp_f, rfc_ssrc_rtp_f, rfc_begin_seq_f, rfc_num_reports_f,
-                         rfc_pkts_block_f, rfc_pkt_block_f, rfc_pkt_rcvd_f, rfc_pkt_arrival_offset_f,
+                         rfc_pkts_block_f, rfc_pkt_block_f, rfc_pkt_rcvd_f, rfc_pkt_ecn_f, rfc_pkt_arrival_offset_f,
                          rfc_report_ts_f }
 
     rfc_not_received_e = ProtoExpert.new("rfc8888.pkt_not_rcvd", "Packet not yet received",
@@ -88,7 +89,7 @@ do
         local ssrc_rtp = buffer(c, 4)
         local begin_seq = buffer(c+4, 2)
         local num_reports = buffer(c+6, 2)
-        local num_reports_actual = num_reports:uint() + 1
+        local num_reports_actual = num_reports:uint()+1
         local tree_end = math.min(buffer:len()-c, 8+num_reports_actual*2)
 
         local tree = root:add(rfc_ssrc_block_rtp_f, buffer(c, tree_end), ssrc_rtp:uint())
@@ -96,44 +97,49 @@ do
         tree:add(rfc_begin_seq_f, begin_seq)
         tree:add(rfc_num_reports_f, num_reports, num_reports_actual)
 
-        local pkts_tree = tree:add(rfc_pkts_block_f, buffer(c, tree_end))
-        local i_pkt = 0
-        local not_rcvd = {}
         c = c + 8
+
+        local pkts_tree = tree:add(rfc_pkts_block_f, buffer(c, tree_end-8))
+        local i_pkt = 0
+        local not_rcvd_list = {}
+        local not_rcvd_t = {}
         while (num_reports_actual > i_pkt) do
             if c + 2 > buffer:len() then break end
+            local pkt_seq = begin_seq:uint()+i_pkt
 
             local received = bit.band(buffer(c,1):uint(), 0x80)
-            -- local ecn = ...
+            local ecn = bit.band(buffer(c,1):uint(), 0x60)
             local arrival_offset = bit.bxor(buffer(c,2):uint(), 0x8000)
             local arrival_time = arrival_offset * (1/1024)
 
             local pkt_tree_text = ""
             if (received ~= 0) then
-                pkt_tree_text = string.format("%d received at %f (offset %f ms)", begin_seq:uint()+i_pkt, report_ts+arrival_time, arrival_time*1000)
+                pkt_tree_text = string.format("%d received at %f (offset %f ms)", pkt_seq, report_ts+arrival_time, arrival_time*1000)
             else
-                pkt_tree_text = string.format("%d not yet received", begin_seq:uint()+i_pkt)
-                table.insert(not_rcvd, begin_seq:uint()+i_pkt)
+                pkt_tree_text = string.format("%d not yet received", pkt_seq)
+                table.insert(not_rcvd_list, pkt_seq)
+                not_rcvd_t[pkt_seq] = pkt_seq
             end
             local pkt_tree = pkts_tree:add(rfc_pkt_block_f, buffer(c,2), pkt_tree_text)
             pkt_tree:add(rfc_pkt_rcvd_f, buffer(c,1), received)
+            pkt_tree:add(rfc_pkt_ecn_f, buffer(c,1), ecn)
             pkt_tree:add(rfc_pkt_arrival_offset_f, buffer(c,2), arrival_time)
 
             i_pkt = i_pkt + 1
             c = c + 2
         end
 
-        local not_rcvd_str = format_not_rcvd(not_rcvd)
-        if (#not_rcvd > 0) then
+        local not_rcvd_str = format_not_rcvd(not_rcvd_list)
+        if (#not_rcvd_list > 0) then
             tree:add_proto_expert_info(rfc_not_received_e, not_rcvd_str)
         end
 
         -- TODO: not a great place to put this
-        local info_str = "ACKs "..begin_seq:uint().."-"..(begin_seq:uint()+num_reports_actual)..". "..not_rcvd_str
+        local info_str = "ACKs "..begin_seq:uint().."-"..(begin_seq:uint()+num_reports_actual-1)..". "..not_rcvd_str
         pinfo.cols.info:set(info_str)
 
 
-        not_rcvd_tbl[pinfo.number][ssrc_rtp:uint()] = not_rcvd
+        not_rcvd_tbl[pinfo.number][ssrc_rtp:uint()] = not_rcvd_t
         ack_range_tbl[pinfo.number][ssrc_rtp:uint()] = { begin_seq:uint(), begin_seq:uint()+num_reports_actual }
         detect_ofo(buffer, pinfo, tree, ssrc_rtp:uint(), report_ts)
 
@@ -160,7 +166,7 @@ do
 
     prev_report_ts_tbl = {} -- { ssrc: report timestamp }
     prev_report_number_tbl = {} -- { ssrc: pinfo.number }
-    not_rcvd_tbl = {} -- { pinfo.number: { ssrc: [not_rcvd] } }
+    not_rcvd_tbl = {} -- { pinfo.number: { ssrc: {not_rcvd: not_rcvd} } }
     ack_range_tbl = {} -- { pinfo.number: { ssrc: [begin_seq, end_seq] } }
     ofo_tbl = {} -- { pinfo.number: { ssrc: [number] } }
 
